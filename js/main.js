5ee15375bbf0ea99e0e1a6c3f5962b58830e79f8
Jelena <jelena33.bliss@gmail.com>
2026-07-02 09:55:13 +0200

    cssMain

--- js/main.js
+++ js/main.js
@@ -18,3 +18,10 @@ var swiper = new Swiper(".services-content", {
         prevEl: ".swiper-button-prev",
       },
     });
+
+    // Header
+    let header = document.querySelector('header')
+
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
